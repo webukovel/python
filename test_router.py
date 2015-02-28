46e90bddbcafea6fb207fe674a3e1b9e9f16155d
pavelperkin <perkinpv@gmail.com>
2015-02-28 18:07:07 +0200

    typo

--- test_router.py
+++ test_router.py
@@ -7,4 +7,4 @@ def test_router():
 	eq_(route_from(17), {'time': 23, 'checkpoints': [17,16,13,11,2,6]})
 	eq_(route_from(0), {'time': 7, 'checkpoints': [0,1,2,6]})
 	eq_(route_from(10), {'time': 20, 'checkpoints': [10,9,4,3,6]})
-	eq_(route_from(7), {'time': 20, 'checkpoints': [7,12,11,2,6]})
+	eq_(route_from(7), {'time': 11, 'checkpoints': [7,12,11,2,6]})
